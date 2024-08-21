18674b144b131baed4b2d7d34cbe9bd3be543997
kostya <tanov.konstanti@ynadex.ru>
2024-08-21 20:17:52 +0300

    new_contain

--- Metaprogramming/src/metaproga/contain.h
+++ Metaprogramming/src/metaproga/contain.h
@@ -1,29 +1,22 @@
 #ifndef METAPROGA_CONTAIN_H
 #define METAPROGA_CONTAIN_H
-#include <typeinfo>
+
 
 template<typename ... T>
 struct Pack{
 
-    constexpr int size() {
-        return (sizeof ... (T));
+    template<typename item>
+    constexpr bool initialize(){
+        return std::disjunction<std::is_same<T, item> ...>{};
     }
 
-    constexpr std::array<const char*, sizeof ... (T)> initialize(){
-        return {typeid(T).name() ...};
-    }
 };
 
 template<typename Pack, typename item>
 constexpr bool contain()
 {
     Pack a;
-    constexpr int lenth = a.size();
-    std::array<const char*, lenth> type = a.initialize();
-    for (int i = 0; i < lenth; ++i) {
-        if (typeid(item).name() == type[i]){return true;}
-    }
-    return false;
+    return (a. template initialize<item>());
 }
 
 #endif
