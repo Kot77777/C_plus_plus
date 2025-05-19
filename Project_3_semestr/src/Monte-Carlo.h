d101cb603ca339203bdac755d5a28d31bcffc202
GlebLarkin <larkin.gs@phystech.edu>
2025-05-19 14:41:45 +0300

    ultra mega monte carlo fix

--- Project_3_semestr/src/Monte-Carlo.h
+++ Project_3_semestr/src/Monte-Carlo.h
@@ -2,12 +2,13 @@
 
 #include <random>
 #include <vector>
-#include "Type.h"
+
 
 constexpr double R1 = 7e6;
 constexpr double mu = 3.986e14;
 static const double omega = std::sqrt(mu / (R1 * R1 * R1));
 static const double V1 = std::sqrt(mu / R1);
+
 
 struct cond
 {
@@ -15,28 +16,34 @@ struct cond
   std::vector<double> R2_;
   std::vector<double> V1_ = {0, V1, 0};
   std::vector<double> V2_ = {0, V1, 0};
-  
-  std::vector<double> delta_r = {R2_[0] - R1_[0], R2_[1] - R1_[1], R2_[2] - R1_[2]};
+  std::vector<double> delta_r;
 
-  cond(std::vector<double> R2) 
-  : R2_(R2)
+  cond(const std::vector<double> R2)
+      : R2_(R2)
   {
     V2_[0] -= omega * (R2_[1] - R1_[1]);
     V2_[1] += omega * (R2_[0] - R1_[0]);
+
+    delta_r = 
+    {
+      R2_[0] - R1_[0],
+      R2_[1] - R1_[1],
+      R2_[2] - R1_[2]
+    };
   }
 };
 
 
-// we need sigma for R1, delta_R and V, not for R1, R2 and V
 struct random_init_cond
 {
 public:
   random_init_cond(const double sigma_delta_R,
                    const size_t init_condition_arr_size)
       : sigma_delta_R_(sigma_delta_R),
-        init_condition_arr_size_(init_condition_arr_size) {}
-    
-  std::vector<cond> Get_init_cond(double R1, std::vector<double> omega) const
+        init_condition_arr_size_(init_condition_arr_size),
+        gen_(std::random_device{}()) {}
+
+  std::vector<cond> Get_init_cond() const
   {
     std::vector<cond> init_cond(init_condition_arr_size_);
 
@@ -46,28 +53,27 @@ public:
       R2[0] += R1;
 
       cond c(R2);
-
       init_cond[i] = c;
     }
 
     return init_cond;
   }
 
-
 private:
   double sigma_delta_R_;
   size_t init_condition_arr_size_;
+  mutable std::mt19937 gen_;
 
-  std::vector<double> generate_normal_randoms(const double sigma, const size_t vector_size) const
+  std::vector<double> generate_normal_randoms(double sigma, size_t vector_size) const
   {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::normal_distribution<> d(0.0, std::sqrt(sigma));
+    std::normal_distribution<> dist(0.0, sigma);
+    std::vector<double> vec(vector_size);
 
-    std::vector<double> random_numbers(vector_size);
+    for (size_t i = 0; i < vector_size; ++i) 
+    {
+      vec[i] = dist(gen_);
+    }
 
-  for (size_t i = 0; i < vector_size; ++i) { random_numbers[i] = d(gen); }
-
-  return random_numbers;
+    return vec;
   }
 };
