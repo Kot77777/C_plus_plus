671d546ca38dc039ee8f486e7b7fb4b155b8e712
GlebLarkin <larkin.gs@phystech.edu>
2025-05-19 14:30:40 +0300

    ultra monte carlo fix

--- Project_3_semestr/src/Monte-Carlo.h
+++ Project_3_semestr/src/Monte-Carlo.h
@@ -4,14 +4,26 @@
 #include <vector>
 #include "Type.h"
 
+constexpr double R1 = 7e6;
+constexpr double mu = 3.986e14;
+static const double omega = std::sqrt(mu / (R1 * R1 * R1));
+static const double V1 = std::sqrt(mu / R1);
 
 struct cond
 {
-  std::vector<double> R1;
-  std::vector<double> R2;
-  std::vector<double> V;
+  std::vector<double> R1_ = {R1, 0, 0};
+  std::vector<double> R2_;
+  std::vector<double> V1_ = {0, V1, 0};
+  std::vector<double> V2_ = {0, V1, 0};
+  
+  std::vector<double> delta_r = {R2_[0] - R1_[0], R2_[1] - R1_[1], R2_[2] - R1_[2]};
 
-  cond() : R1(2), R2(3), V(3) {}
+  cond(std::vector<double> R2) 
+  : R2_(R2)
+  {
+    V2_[0] -= omega * (R2_[1] - R1_[1]);
+    V2_[1] += omega * (R2_[0] - R1_[0]);
+  }
 };
 
 
@@ -24,20 +36,16 @@ public:
       : sigma_delta_R_(sigma_delta_R),
         init_condition_arr_size_(init_condition_arr_size) {}
     
-  std::vector<cond> Get_init_cond(double R1, double omega) const
+  std::vector<cond> Get_init_cond(double R1, std::vector<double> omega) const
   {
     std::vector<cond> init_cond(init_condition_arr_size_);
 
     for (size_t i = 0; i < init_condition_arr_size_; ++i)
     {
-      cond c;
-      c.R1 = std::vector<double> {R1, 0, 0};
+      std::vector<double> R2 = generate_normal_randoms(sigma_delta_R_, 3);
+      R2[0] += R1;
 
-      c.R2 = generate_normal_randoms(sigma_delta_R_, 3);
-      for (size_t j = 0; j < 2; ++j) { c.R2[j] += c.R1[j]; }
-
-      double mu = 3.986e14;
-      c.V = std::vector<double> {0, std::sqrt(mu / R1), 0};
+      cond c(R2);
 
       init_cond[i] = c;
     }
